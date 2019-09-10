9f7cda5179af768c88406ce48ea6ec0a51857f44
mohamedosama2074 <iosengks74@gmail.com>
2019-09-10 18:54:49 +0200

    DownloadImage

--- DownloadImage/ViewController.swift
+++ DownloadImage/ViewController.swift
@@ -10,11 +10,79 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageDownloaded: UIImageView!
+    
+    @IBOutlet weak var progressbar: UIActivityIndicatorView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
     }
 
-
+    @IBAction func btn_download(_ sender: Any) {
+        
+        let url = URL(string: "https://scontent-hbe1-1.xx.fbcdn.net/v/t1.0-9/69057465_771053816683983_1600980636183560192_n.jpg?_nc_cat=104&_nc_oc=AQl2Jk8DObnoqAo7kpobZoFuQcaoaoSwAFszRWTtIrMZ8QFYL8fdmwPaoKZtZ3StDyc&_nc_ht=scontent-hbe1-1.xx&oh=2fda9174e2ca74aacce22323d69465e3&oe=5DF372FF")
+    
+        if let url = url {
+            
+            
+            downloadTask(imageLink: url)
+            
+        }
+        
+        
+        
+        
+      
+        }
+        
+    
+  
+    
+    fileprivate func progressCheck(shouldSpin status:Bool){
+        
+        if status == true {
+            
+            progressbar.isHidden = false
+            
+            progressbar.startAnimating()
+            
+        }else{
+            
+            progressbar.isHidden = true
+            progressbar.stopAnimating()
+        }
+        
+    }
+    
+    private func downloadTask(imageLink url:URL){
+        progressCheck(shouldSpin: false)
+        let session = URLSession.shared
+        
+        session.dataTask(with: url) { (data, response, error) in
+            
+            if let error = error {
+                
+                print(error)
+            }
+            
+            DispatchQueue.main.async {
+             
+                if let data = data{
+                    
+                    self.imageDownloaded.image = UIImage(data: data)
+                    self.progressCheck(shouldSpin: false)
+                }
+                
+            }
+            
+        }.resume()
+        
+        
+    }
+    
 }
 
